5e5768d8b7fbc4fd8da6dc7624962bf14607da77
Zippy-boy <zippy-boy@outlook.com>
2022-05-10 07:54:25 +0100

    2
    app.py

--- src/shutdown/app.py
+++ src/shutdown/app.py
@@ -1,26 +1,16 @@
 """
 Shutdown timer
 """
+from ctypes import alignment
+from tkinter import CENTER
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 import asyncio
-import datetime
+from datetime import datetime
 
 
 class shutdown(toga.App):
-    async def A_shutdown(self, widget):
-        # get the value of A_hour_input and A_minute_input
-        A_hour = self.A_hour_input.value
-        A_minute = self.A_minute_input.value
-        A_time = "%s:%s" % (A_hour, A_minute)
-         # get the time 
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        while A_time != current_time:
-            ''' SHUT DOWN'''
-            print("asd")
-
     def startup(self):
         """
         Construct and show the Toga application.
@@ -29,7 +19,8 @@ class shutdown(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box(style=Pack(direction=COLUMN))
+        self.veiw_box = toga.Box(style=Pack(direction=COLUMN))
+        self.main_box = toga.Box(style=Pack(direction=COLUMN))
         TIME_SELECTION = toga.Box(style=Pack(direction=COLUMN))
         AT_TIME = toga.Box(style=Pack(direction=COLUMN))
         T_hour_box = toga.Box(style=Pack(direction=ROW))
@@ -53,25 +44,25 @@ class shutdown(toga.App):
         self.A_minute_input = toga.NumberInput(style=Pack(padding=5, font_size=14, flex=1, height=20), min_value=0, max_value=59)
     
         T_hour_box.add(T_hour_label)
-        T_hour_box.add(T_hour_input)
+        T_hour_box.add(self.T_hour_input)
         T_minute_box.add(T_minute_label)
-        T_minute_box.add(T_minute_input)
+        T_minute_box.add(self.T_minute_input)
         T_second_box.add(T_second_label)
-        T_second_box.add(T_second_input)
+        T_second_box.add(self.T_second_input)
         A_hour_box.add(A_hour_label)
-        A_hour_box.add(A_hour_input)
+        A_hour_box.add(self.A_hour_input)
         A_minute_box.add(A_minute_label)
-        A_minute_box.add(A_minute_input)
+        A_minute_box.add(self.A_minute_input)
 
         T_button = toga.Button(
             "Enter Time",
             on_press=self.T_shutdown,
-            style=Pack(padding=5, width=500, height=30, font_size=15),
+            style=Pack(padding=5, flex=1, height=30, font_size=15),
         )
         A_button = toga.Button(
             "Enter Time",
             on_press=self.A_shutdown,
-            style=Pack(padding=5, width=500, height=30, font_size=15),
+            style=Pack(padding=5, flex=1, height=30, font_size=15),
         )
 
         TIME_SELECTION.add(T_hour_box)
@@ -95,16 +86,60 @@ class shutdown(toga.App):
 
         
 
-        main_box.add(container)
+        self.veiw_box.add(container)
+        self.main_box.add(self.veiw_box)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = self.main_box
         self.main_window.show()
 
+    async def A_shutdown(self, widget):
+        # get the value of A_hour_input and A_minute_input
+        A_hour = str(self.A_hour_input.value)
+        A_minute = str(self.A_minute_input.value)
+        A_hour = A_hour.zfill(2)
+        A_minute = A_minute.zfill(2)
+        A_time = "%s:%s" % (A_hour, A_minute)
+        if A_time == "None:None":
+            A_time = "00:00"
+         # get the time 
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
 
+        # clear the window
+        # create a label that shows the time and the time when shutdown
+        self.main_box.remove(self.veiw_box)
+        time_label = toga.Label("Current Time: %s" % current_time, style=Pack(padding=0, font_size=15))
+        self.main_box.add(time_label)
+        # get the current time in seconds
+        A = current_time.split(':')
+        temp = int(A[0])
+        temp2 = int(A[1])
+        current_time_seconds = temp * 3600 + temp2 * 60
 
+        # get the time when shutdown in seconds
+        A = A_time.split(':')
+        temp = int(A[0])
+        temp2 = int(A[1])
+        A_time_seconds = temp * 3600 + temp2 * 60
 
+        difference = A_time_seconds - current_time_seconds
+        if difference < 0:
+            difference = difference + 86400
+
+        print(difference)
+        progress = toga.ProgressBar(max=difference, value=1)
+
+        
+        while A_time != current_time:
+            ''' SHUT DOWN'''
+            now = datetime.now()
+            current_time = now.strftime("%H:%M")
+            print("asd")
+            await asyncio.sleep(2)  
             
+
+        print("!!!TIME!!!")         
 
 
     async def T_shutdown(self, widget):
